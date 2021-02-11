25f09a58f92afcaaaa17bcb350149ad6791f1796
FabioMusi04 <76118020+FabioMusi04@users.noreply.github.com>
2021-02-11 17:43:30 +0100

    Add files via upload

---
+++ MusicPlayer/MusicPlayer/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace MusicPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBoxMusica = new System.Windows.Forms.ListBox();
+            this.btnSceglimusica = new System.Windows.Forms.Button();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBoxMusica
+            // 
+            this.listBoxMusica.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.listBoxMusica.Font = new System.Drawing.Font("MS UI Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxMusica.ForeColor = System.Drawing.Color.Red;
+            this.listBoxMusica.FormattingEnabled = true;
+            this.listBoxMusica.Location = new System.Drawing.Point(745, 14);
+            this.listBoxMusica.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.listBoxMusica.Name = "listBoxMusica";
+            this.listBoxMusica.Size = new System.Drawing.Size(318, 342);
+            this.listBoxMusica.Sorted = true;
+            this.listBoxMusica.TabIndex = 1;
+            this.listBoxMusica.SelectedIndexChanged += new System.EventHandler(this.listBoxMusica_SelectedIndexChanged);
+            // 
+            // btnSceglimusica
+            // 
+            this.btnSceglimusica.BackColor = System.Drawing.SystemColors.Desktop;
+            this.btnSceglimusica.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSceglimusica.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnSceglimusica.Location = new System.Drawing.Point(827, 381);
+            this.btnSceglimusica.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnSceglimusica.Name = "btnSceglimusica";
+            this.btnSceglimusica.Size = new System.Drawing.Size(237, 57);
+            this.btnSceglimusica.TabIndex = 2;
+            this.btnSceglimusica.Text = "Select Song";
+            this.btnSceglimusica.UseVisualStyleBackColor = false;
+            this.btnSceglimusica.Click += new System.EventHandler(this.btnSceglimusica_Click);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 12);
+            this.axWindowsMediaPlayer1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(725, 396);
+            this.axWindowsMediaPlayer1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Aqua;
+            this.label1.Location = new System.Drawing.Point(28, 411);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 27);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Develomped by Fabio Musitelli";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(1067, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.btnSceglimusica);
+            this.Controls.Add(this.listBoxMusica);
+            this.Font = new System.Drawing.Font("MS UI Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Music Player App";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBoxMusica;
+        private System.Windows.Forms.Button btnSceglimusica;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MusicPlayer/MusicPlayer/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace MusicPlayer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        String[] paths, files;
+        private void btnSceglimusica_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Sceglimusica = new OpenFileDialog();
+            //selezioni piu files
+            Sceglimusica.Multiselect = true;
+            if (Sceglimusica.ShowDialog()==System.Windows.Forms.DialogResult.OK)
+            {
+                files = Sceglimusica.SafeFileNames;//Salva nome del file nell array
+                paths = Sceglimusica.FileNames;//Salva la path per il file musica nel array
+                //Visualizza ora in listbox
+                for (int i = 0; i < files.Length; i++)
+                {
+                    listBoxMusica.Items.Add(files[i]); //visualizza musica nella list 
+                }
+            }
+        }
+
+        private void listBoxMusica_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Codice per avviare musica
+            axWindowsMediaPlayer1.URL = paths[listBoxMusica.SelectedIndex];//click per avviare musica nel windows media player
+        }
+
+    }
+}
